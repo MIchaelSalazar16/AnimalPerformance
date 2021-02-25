3f0f46de6e5ce89c11f81909478a5e7580d4bb62
MIchaelSalazar16 <michaelsalazar493@gmail.com>
2021-02-25 05:27:28 -0500

    inicios_V0.0

--- appAnimalPerformance/forms.py
+++ appAnimalPerformance/forms.py
@@ -3,24 +3,24 @@ from .models import Rendimiento,LoteAnimal,Animal,Producto,Usuario
 
 class AnimalForm(ModelForm):
     class Meta:
-        model = Rendimiento
-        fields = ['idAnimal','nombreAnimal','peso_animal']
+        model = Animal
+        fields = ['idAnimal','nombreAnimal','peso_animal',"precio_costo"]
 
 class LoteAnimalForm(ModelForm):
     class Meta:
-        model = Rendimiento
+        model = LoteAnimal
         fields = ['idLoteAnimal','idAnimal','peso_lote']
 
 class ProductoForm(ModelForm):
     class Meta:
-        model = Rendimiento
+        model = Producto
         fields = ['idProducto','idAnimal','nombreProducto','peso_producto',
                     'utilidad_producto','precio_costo','precio_venta',
                     'porcentaje_peso','total_costo_producto','total_venta_producto','margen_utilidad_producto']
 
 class UsuarioForm(ModelForm):
     class Meta:
-        model = Rendimiento
+        model = Usuario
         fields = ['idUsuario','cedula','nombres','apellidos','correo','password']
 
 class RendimientoForm(ModelForm):
--- appAnimalPerformance/views.py
+++ appAnimalPerformance/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.shortcuts import redirect,render
 from .models import Animal,Producto,Rendimiento,LoteAnimal,Usuario
-from .forms import AnimalForm,LoteAnimalForm,ProductoForm,RendimientoForm,UsuarioForm
+#from .forms import AnimalForm,LoteAnimalForm,ProductoForm,RendimientoForm,UsuarioForm
 from django.core.files.uploadedfile import SimpleUploadedFile
 import json as simplejson
 from django.http import HttpResponse,HttpResponseRedirect
